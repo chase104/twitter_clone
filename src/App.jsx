dd42270ca296aae8c1dcc182f8f12ec4f415b5b3
Chase Van Halen <cvanhalen@perscholas.org>
2023-06-21 16:50:22 -0500

    react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import Form from './components/form'
-import DisplayTweets from './components/display_tweets'
-import axios from 'axios'
+import { useEffect, useState } from 'react';
+import './App.css';
+import Form from './components/form';
+import DisplayTweets from './components/display_tweets';
+import axios from 'axios';
+import { Link, Navigate, Route, Routes } from 'react-router-dom';
+import Signup from './pages/Signup';
 
 function App() {
 
@@ -19,15 +21,28 @@ function App() {
       })
     }, []);
 
+    const Home = () => {
+      return (
+        <>
+          <Form   tweets={tweets} setTweets={setTweets}  />
+          <DisplayTweets tweets={tweets} setTweets={setTweets} />
+        </>
+      )
+    }
+
   return (
     <>
     <h1>My Twitter Clone</h1>
-  {/* create data  DONE*/}
-    <Form   tweets={tweets} setTweets={setTweets}  />
-    <DisplayTweets tweets={tweets} setTweets={setTweets} />
-  {/* read data */}
-  {/* update data */}
-  {/* delete data */}
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/signup">SignUp</Link>
+    </nav>
+    {/* PAGES HERE */}
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/signup' element={<Signup />}/>
+      <Route path='/*' element={<Navigate to="/"/>}/>
+    </Routes>
     </>
   )
 }
